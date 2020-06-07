2f20005ccb257e3b8d0405f07f4a1d8fa6a142f2
learnjs2020 <54968697+learnjs2020@users.noreply.github.com>
2020-06-07 01:17:16 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-echo "-- Start script  -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /tmp/cc.log
+markfile="/var/_cronData/mark.data"
+folder="/var/_cronData/inBound"
 while true; do 
    sleep 5
-   echo "-- Ran script  -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /tmp/cc.log
+   for file in $(find $markfile -not -newermt '-300 seconds') ;do
+      rm -fr $file >/dev/null 2>&1
+   done
+   
+   for f in "$folder"/*; do
+     if [ -f "$markfile" ]; then
+       break;
+     fi
+     if [ -f "$f" ]; then
+       echo $f >  $markfile
+
+       fn=/tmp/$(basename $f)_$(date +%s%N).data
+       echo "-- Start process $f -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /var/_cronData/dataCron.log
+       cmdd="mv -fr $f $fn && rm -fr $f && sh /var/__localChannel/dataScript.sh $fn && rm -fr $fn && rm -fr $markfile "
+       eval " $cmdd >> /var/_cronData/dataCron.log"
+       echo "-- Done $f -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /var/_cronData/dataCron.log
+     fi
+   done
 done
