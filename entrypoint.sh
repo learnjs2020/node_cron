5bc2db8a42361f15e2d9d5448d6791126aa2c018
learnjs2020 <54968697+learnjs2020@users.noreply.github.com>
2020-06-06 22:35:19 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "-- Start script  -- at $(date +"%m/%d/%Y %H:%M:%S")"
+echo "-- Start script  -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /tmp/cc.log
 while true; do 
    sleep 1
-   echo "-- Ran script  -- at $(date +"%m/%d/%Y %H:%M:%S")"
+   echo "-- Ran script  -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /tmp/cc.log
 done
