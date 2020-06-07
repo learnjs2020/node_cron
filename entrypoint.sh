accbedda385c3544f3a5d2a1957e4ee0e4992707
learnjs2020 <54968697+learnjs2020@users.noreply.github.com>
2020-06-06 21:55:34 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "-- Start script  -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /tmp/clog.log
 while true; do 
    echo "-- Ran script  -- at $(date +"%m/%d/%Y %H:%M:%S")" >> /tmp/clog.log
 done
